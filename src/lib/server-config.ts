96a833086ee1a17c22560cfbb0bb5d3aa108677e
aricahya88-source <ari.cahya88@gmail.com>
2026-09-08 20:45:55 +0700

    Update server-config.ts

--- src/lib/server-config.ts
+++ src/lib/server-config.ts
@@ -4,7 +4,7 @@
  * Spreadsheet ID dan Folder ID hanya berada di Apps Script.
  */
 export const SERVER_CONFIG = Object.freeze({
-  APPS_SCRIPT_URL: process.env.APPS_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbw0RH8JHQlYCYge24oRto_S2dD9a2pv6mNA3j2BcyDbpxRdE2zFaeHn-kY1MMelhq8-/exec',
+  APPS_SCRIPT_URL: process.env.APPS_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbwfhML7b35SP-Co1NWE2OkYJ1broxWfXavyu2jO6SGnhINnHxg2hzcaQ1ECA83zwiXs/exec',
   REQUEST_TIMEOUT_MS: 60000,
   MAX_UPLOAD_BYTES: 3 * 1024 * 1024
 });
